dad47e21e0c0e04979e12b2cb1152e37683e2527
KeleD2000 <k.dominik2000@gmail.com>
2023-12-30 15:07:46 +0100

    12.30. sender list done

--- src/app/player/player-messages/player-messages.component.ts
+++ src/app/player/player-messages/player-messages.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { delay } from 'rxjs/operators';
+import { FileService } from 'src/app/services/file.service';
+import * as AOS from 'aos';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-player-messages',
@@ -6,5 +10,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./player-messages.component.css']
 })
 export class PlayerMessagesComponent {
+  receiverId: number = 0;
+  senderProfPic: string = '';
+  image: any;
+  senderArray: any[] = [];
+  senderObject: any = {};
 
+  constructor(private messageService: MessagesService,  private fileService: FileService){}
+
+  ngOnInit(){
+    const username = localStorage.getItem('isLoggedin');
+    let current = username?.replace(/"/g, '');
+  
+    this.fileService.getCurrentUser(current).subscribe((prof) => {
+      for(const [key, value] of Object.entries(prof)){
+        if(key === 'id'){
+          this.receiverId = value;
+          this.getSendersWithDelay();
+        }
+      }
+    });
+  }
+  
+  getSendersWithDelay() {
+    this.messageService.getSenders(this.receiverId)
+      .pipe(
+        delay(1000)
+      )
+      .subscribe((receiver) => {
+        for (let i in receiver) {
+          const senderObject: any = {
+            name: receiver[i].username,
+            content: receiver[i].message_content,
+            image: ""
+          };
+          
+          this.fileService.getProfilePicBlob(senderObject.name).subscribe(
+            (response: any) => {
+              if (response) {
+                const reader = new FileReader();
+                reader.onload = () => {
+                  senderObject.image = reader.result as string;
+                };
+                reader.readAsDataURL(new Blob([response], { type: 'image/png' }));
+              }
+            },
+            (error) => {
+              console.error('Error fetching profile picture:', error);
+            }
+          );
+      
+          this.senderArray.push(senderObject);
+        }
+      
+        console.log(this.senderArray);
+      });
+      
+  }
+  
+
+  ngAfterViewInit() {
+    AOS.init({
+      once: true
+    });
+  }
 }
---
+++ src/app/services/messages.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { FileService } from './file.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessagesService {
+
+  private baseUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  getSenders(receiverId: number): Observable<any>{
+    return this.http.get(`${this.baseUrl}/api/messages/latest/` + receiverId);
+  }
+
+
+}
